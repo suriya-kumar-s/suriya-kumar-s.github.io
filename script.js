24b8d6c8eb883ad44c049354a0b03c699a9a83a4
SURIYA KUMAR S <suriyakumar342003@gmail.com>
2025-05-05 03:53:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Smooth Scrolling for Navigation Links
-  const navLinks = document.querySelectorAll('.nav-links a');
-  navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-      const targetId = link.getAttribute('href').substring(1); // Get target ID from href
-      const targetElement = document.getElementById(targetId);
-      window.scrollTo({
-        top: targetElement.offsetTop - 50, // Offset to make sure header doesn't cover it
-        behavior: 'smooth'
-      });
-    });
-  });
-});
+
